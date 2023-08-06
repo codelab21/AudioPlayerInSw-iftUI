0290776959113698d249815ed3c222cc5d1c2682
codelab21 <codelab21@outlook.com>
2023-08-06 13:24:41 +0300

    Initial Commit

---
+++ AudioPlayerDemo/AudioPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerDemoApp.swift
+//  AudioPlayerDemo
+//
+//  Created by Eymen on 6.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
